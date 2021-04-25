4a6075c5969442ede1c54c349ae88f734caad9fb
zourit77 <zourit77@free.fr>
2021-04-25 17:24:19 +0200

    Update graphique.sh

--- graphique.sh
+++ graphique.sh
@@ -6,26 +6,26 @@ LISTE=("[g] generique" "[n] nvidia" "[t] nvidia-lts" "[b] hybride-bumblebee")  #
 select CHOIX in "${LISTE[@]}" ; do
     case $REPLY in
         1|g)
-        pacman -S xorg xorg-server xdg-user-dirs xf86-video-vesa --noconfirm
+        pacman -S pacman -S xorg-{server,xinit,apps} xf86-input-libinput xdg-user-dirs xf86-video-vesa --noconfirm
         echo "appuyer pour continuer..."
         break
         sh ./noyau.sh
         ;;
         2|n)
-        pacman -S xorg xorg-server mesa-demos nvidia nvdia-settings nvidia-libgl libva-vdpau-driver --noconfirm
+        pacman -S pacman -S xorg-{server,xinit,apps} xf86-input-libinput xdg-user-dirs xf86-video-vesa mesa-demos nvidia nvdia-settings nvidia-libgl libva-vdpau-driver --noconfirm
         systemctl enable nvidia-persistenced.service
         echo "appuyer pour continuer..."
         break
         sh ./noyau.sh
         ;;
         3|t)
-         pacman -S xorg xorg-server mesa-demos nvidia-lts nvdia-settings nvidia-libgl libva-vdpau-driver --noconfirm
+         pacman -S pacman -S xorg-{server,xinit,apps} xf86-input-libinput xdg-user-dirs xf86-video-vesa mesa-demos nvidia-lts nvdia-settings nvidia-libgl libva-vdpau-driver --noconfirm
         systemctl enable nvidia-persistenced.service
         echo "appuyer pour continuer..."
         break
         sh ./noyau.sh
         3|b)
-         pacman -S xorg xorg-server mesa-demos mesa xf86-video-intel bumblebee nvidia nvdia-settings nvidia-libgl libva-vdpau-driver --noconfirm
+         pacman -S pacman -S xorg-{server,xinit,apps} xf86-input-libinput xdg-user-dirs xf86-video-vesa mesa-demos mesa xf86-video-intel bumblebee nvidia nvdia-settings nvidia-libgl libva-vdpau-driver --noconfirm
         gpasswd -a  $user bumblebee
         systemctl enable bumblebeed.service
         echo "appuyer pour continuer..."
