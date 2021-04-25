ce28b975dc394a2350b5d7b8d19d5d9d01a36e29
zourit77 <zourit77@free.fr>
2021-04-25 16:17:29 +0200

    Create lang.sh

---
+++ lang.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+# Paramètre la langue systeme
+echo KEYMAP=fr-latin9 > /etc/vconsole.conf
+echo LANG=fr_FR.UTF-8 > /etc/locale.conf
+echo Archy > /etc/hostname
+mv /etc/locale.gen /etc/locale.gen.old
+echo fr_FR.UTF-8 UTF-8 /etc/locale.gen
+#sed -i -e 's/#fr_FR.UTF-8 UTF-8/fr_FR.UTF-8 UTF-8/g' /etc/locale.gen
+locale-gen
+export LANG=fr_FR.UTF-8
+echo "langue parametrée en francais !"
+
+# Implémente la zone géographique
+ln -sf /usr/share/zoneinfo/Europe/Paris /etc/localtime
+hwclock --systohc --utc
+echo "zone geographique parametrée"
+
