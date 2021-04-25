88e65a4ee1c82c7c7c9ba1228c2e9741b07c665d
zourit77 <zourit77@free.fr>
2021-04-25 18:42:07 +0200

    Update lang.sh

--- lang.sh
+++ lang.sh
@@ -4,9 +4,9 @@
 echo KEYMAP=fr-latin9 > /etc/vconsole.conf
 echo LANG=fr_FR.UTF-8 > /etc/locale.conf
 echo Archy > /etc/hostname
-mv /etc/locale.gen /etc/locale.gen.old
-echo fr_FR.UTF-8 UTF-8 /etc/locale.gen
-#sed -i -e 's/#fr_FR.UTF-8 UTF-8/fr_FR.UTF-8 UTF-8/g' /etc/locale.gen
+#mv /etc/locale.gen /etc/locale.gen.old
+#echo fr_FR.UTF-8 UTF-8 /etc/locale.gen
+sed -i -e 's/#fr_FR.UTF-8 UTF-8/fr_FR.UTF-8 UTF-8/g' /etc/locale.gen
 locale-gen
 export LANG=fr_FR.UTF-8
 echo "langue parametrée en francais !"
