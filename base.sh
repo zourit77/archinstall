e94e39087101496243bd2a7df8c5ab589c7940ca
zourit77 <zourit77@free.fr>
2021-04-25 16:34:48 +0200

    Update base.sh

--- base.sh
+++ base.sh
@@ -3,8 +3,7 @@
 echo "=================================="
 echo "installation de la base du systeme"
 echo "=================================="
-read -p "quel kernel vouslez vous installer ?" noyau
-pacstrap /mnt base base-devel pacman-contrib $noyau $noyau-headers linux-firmware nano grub
+
 pacstrap /mnt zip unzip p7zip nano mc alsa-utils syslog-ng mtools dosfstools lsb-release ntfs-3g exfat-utils bash-completion dialog lzop networkmanager
 
 # inscription des points de montage
