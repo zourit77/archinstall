20d4b6c8866f94578abf402c38669f9990bbac84
zourit77 <zourit77@free.fr>
2021-04-25 16:45:42 +0200

    Update noyau.sh

--- noyau.sh
+++ noyau.sh
@@ -1,5 +1,30 @@
 #! /bin/bash
 
-read -p "quel kernel voulez vous installer ?" noyau
+read -p "quel kernel voulez vous installer ? (linux, linux-lts ?)" noyau
 pacman -S  $noyau $noyau-headers linux-firmware nano --noconfirm
 
+echo "voulez-vous installer l'amorçage GRUB en UEFI ?"
+PS3='> '   # le prompt
+LISTE=("[o] oui" "[n]  non")  # liste de choix disponibles
+select CHOIX in "${LISTE[@]}" ; do
+    case $REPLY in
+        1|o)
+        mkdir /boot/efi
+        pacman -S grub efibootmgr --noconfirm
+        grub-install /dev/$disque --recheck
+        grub-mkconfig -o /boot/grub/grub.cfg
+        echo "grub UEFI installé appuyez sur une touche pour continuer..."
+        break
+        sh ./logiciels.sh
+        ;;
+        2|n)
+        pacman -S grub --noconfirm
+        grub-install /dev/$disque
+        grub-mkconfig -o /boot/grub/grub.cfg
+        
+        echo "grub legacy installé appuyez sur une touche pour continuer..."
+        break
+        sh ./logiciels.sh
+        ;;
+    esac
+done
